3e92e1755ed714048bf9fff376a4309e7b07a3f8
asasouza <asasouza.alex@gmail.com>
2015-09-22 20:07:50 -0300

    Testes para classe concreta A e B separados.

--- test/com/edupsousa/designpatterns/strategy/StrategyTest.java
+++ test/com/edupsousa/designpatterns/strategy/StrategyTest.java
@@ -2,17 +2,28 @@ package com.edupsousa.designpatterns.strategy;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StrategyTest {
 
+	ClasseComStrategy objeto;
+	
+	@Before
+	public void initialize() {
+		objeto = new ClasseComStrategy();
+	}
+	
 	@Test
-	public void testarPadraoStrategy() {
-		ClasseComStrategy objeto = new ClasseComStrategy();
-		
+	public void testarPadraoStrategyConcretaA() {
+
 		objeto.defineStrategy(new StrategyConcretaA());
 		assertEquals("Ação realizada por StrategyConcretaA", objeto.usaStrategyParaFazerAlgo());
 		
+	}
+	
+	@Test
+	public void testarPadraoStrategyConcretaB() {
 		objeto.defineStrategy(new StrategyConcretaB());
 		assertEquals("Ação realizada por StrategyConcretaB", objeto.usaStrategyParaFazerAlgo());
 	}
